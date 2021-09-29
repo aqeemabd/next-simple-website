31365dd1f86e13081a3e091bfe74520ae166a57c
Aqeem Abdullah <aqeemabd@gmail.com>
2021-09-29 08:17:44 +0800

    initial

---
+++ pages/ninjas/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import styles from '../../styles/Ninjas.module.css'
+
+// Run at build time
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    return {
+        props: {ninjas: data}
+    }
+}
+
+const Ninjas = ({ninjas}) => {
+    // console.log(ninjas)
+
+    return (
+        <>
+            <Head>
+                <title>Ninja List | All Ninjas</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <h1>All Ninjas</h1>
+            {ninjas.map(ninja => (
+                <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </Link>
+            ))}
+        </>
+    )
+}
+
+export default Ninjas
